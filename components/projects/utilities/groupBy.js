e0655464b1eff3f754dc265ffee8e1d070f340fc
francoromanolosada <francoromano1994@gmail.com>
2021-05-13 20:49:54 +0200

    Added size orderer reducer

--- components/projects/utilities/groupBy.js
+++ components/projects/utilities/groupBy.js
@@ -1,23 +1,44 @@
-const groupBy = (arr, key) => {
-  let grouped
+function groupBy(array, property) {
+  if (property !== 'size') {
+    return array.reduce(function (acc, obj) {
+      let key = obj[property]
+      if (!acc[key]) {
+        // the first time we create the array with each property
+        acc[key] = []
+      }
+      acc[key].push(obj)
+      return acc
+    }, {})
+  } else if (property === 'size') {
+    // group by scale range
+    const s = 1000
+    const m = 10_000
+    const l = 100_000
+    const xl = 1_000_000
 
-  if (key === 'year') {
-    grouped = arr.reduce(
-      (acc, item) => ((acc[item[key]] = [...(acc[item[key]] || []), item]), acc),
-      {}
-    )
-  } else if (key === 'size') {
-    // need to group by scale range
-    return
+    return array.reduce(function (acc, obj) {
+      let key = obj[property]
+
+      if (!acc[s || m || l || xl]) {
+        // the first time we create the array with each property
+        acc[s] = []
+        acc[m] = []
+        acc[l] = []
+        acc[xl] = []
+      }
+
+      if (key <= m) {
+        acc[s].push(obj)
+      } else if (key <= l) {
+        acc[m].push(obj)
+      } else if (key <= xl) {
+        acc[l].push(obj)
+      } else {
+        acc[xl].push(obj)
+      }
+      return acc
+    }, {})
   }
-
-
-  let ordered = []
-  for (const key in grouped) {
-    ordered.push([grouped[key], key])
-  }
-
-  return ordered
 }
 
 export default groupBy
