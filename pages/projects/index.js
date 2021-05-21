4ec8c00a3dccc8ebf5affa7ec4aa5b7cb6760e19
francoromanolosada <francoromano1994@gmail.com>
2021-05-21 11:55:24 +0200

    Deleted dummy data and added correct importations

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -3,71 +3,17 @@ import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 
-import { projectsApi } from '../../lib/projects'
+import { projectsApi } from '../../lib/api'
+import { groupByProperty } from '../../lib/utilities/'
 
-import groupBy from '../../components/projects/utilities/groupBy'
-import Projects from '../../components/projects/Projects'
-import ProjectsOrderer from '../../components/projects/ProjectsOrderer'
-
-const projects = [
-  {
-    id: 1,
-    shortTitle: 'BAT',
-    title: 'Faste Batteri',
-    year: 2010,
-    size: '124000',
-    programmatic: 'Housing',
-    location: 'Copenhaguen, DK',
-    status: 'Idea',
-  },
-  {
-    id: 2,
-    shortTitle: 'TSP',
-    title: 'The Spiral',
-    year: 2012,
-    size: '264775',
-    programmatic: 'Commercial',
-    location: 'New York, USA',
-    status: 'Under construction',
-  },
-  {
-    id: 3,
-    shortTitle: 'BKB',
-    title: 'Back to the future',
-    year: 2016,
-    size: '161874',
-    programmatic: 'Public space',
-    location: 'New York, USA',
-    status: 'Idea',
-  },
-  {
-    id: 4,
-    shortTitle: 'WTF',
-    title: 'Washington stadium',
-    year: 2018,
-    size: '132665',
-    programmatic: 'Body culture',
-    location: 'Washington D.C., USA',
-    status: 'In progress',
-  },
-  {
-    id: 5,
-    shortTitle: 'FCB',
-    title: 'Barcelona stadium',
-    year: 2018,
-    size: '200000',
-    programmatic: 'Body culture',
-    location: 'Barcelona, ESP',
-    status: 'In progress',
-  },
-]
+import { Projects, ProjectsOrderer } from '../../components/projects'
 
 const ProjectsPage = ({ projects }) => {
   const [projectsOrdered, setProjectsOrdered] = useState(null)
   const [groupProjectsBy, setGroupProjectsBy] = useState('year')
 
   useEffect(() => {
-    const groupedProjects = groupBy(projects, groupProjectsBy)
+    const groupedProjects = groupByProperty(projects, groupProjectsBy)
     setProjectsOrdered(groupedProjects)
   }, [groupProjectsBy])
 
