2405765269805e0c2840f55c4f442a336d4a6787
francoromanolosada <francoromano1994@gmail.com>
2021-05-23 23:37:23 +0200

    Added Link and Img components

--- components/projects/ProjectItem.js
+++ components/projects/ProjectItem.js
@@ -1,9 +1,19 @@
 import styled from 'styled-components'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const ProjectItem = ({ project }) => {
+  const LOCAL_URL = process.env.NEXT_PUBLIC_CMS_LOCALHOST + project.icon.url
+
   return (
     <Item id={project.id} data-flip-id={project.shortTitle}>
-      {project.title}
+      <Link href={`/projects/${project.slug}`}>
+        <img
+          src={process.env.NODE_ENV === 'development' ? LOCAL_URL : project.icon.url}
+          width={project.icon.width / 2}
+          height={project.icon.height / 2}
+        />
+      </Link>
     </Item>
   )
 }
