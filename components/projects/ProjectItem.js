8fc27915a348c10c01e86c54b489275dd81a4c62
francoromanolosada <francoromano1994@gmail.com>
2021-05-24 21:53:31 +0200

    Added Tooltip and imported created Icon component

--- components/projects/ProjectItem.js
+++ components/projects/ProjectItem.js
@@ -1,23 +1,49 @@
-import styled from 'styled-components'
 import Link from 'next/link'
 import Image from 'next/image'
+import styled from 'styled-components'
+import { Tooltip } from 'react-tippy'
+
+import Icon from './Icon'
 
 const ProjectItem = ({ project }) => {
-  const LOCAL_URL = process.env.NEXT_PUBLIC_CMS_LOCALHOST + project.icon.url
+  const LOCAL_ICON = process.env.NEXT_PUBLIC_CMS_LOCALHOST + project.icon.url
+  const LOCAL_COVER = process.env.NEXT_PUBLIC_CMS_LOCALHOST + project.cover.url
 
   return (
-    <Item id={project.id} data-flip-id={project.shortTitle}>
-      <Link href={`/projects/${project.slug}`}>
-        <img
-          src={process.env.NODE_ENV === 'development' ? LOCAL_URL : project.icon.url}
-          width={project.icon.width / 2}
-          height={project.icon.height / 2}
-        />
-      </Link>
-    </Item>
+    <Tooltip title={`${project.shortTitle} - ${project.title}`} position="top" size="small" followCursor="true" offset={90} distance={-50}>
+      <Project id={project.id} data-flip-id={project.shortTitle}>
+        <Link href={`/projects/${project.slug}`}>
+          <Animation>
+            <Icon
+              src={process.env.NODE_ENV === 'development' ? LOCAL_ICON : project.icon.url}
+              width={project.icon.width / 2.2}
+              height={project.icon.height / 2.2}
+              srcOnHover={process.env.NODE_ENV === 'development' ? LOCAL_COVER : project.cover.url}
+            />
+          </Animation>
+        </Link>
+        <Title>{project.shortTitle}</Title>
+      </Project>
+    </ Tooltip>
   )
 }
 
 export default ProjectItem
 
-const Item = styled.article``
+const Project = styled.li`
+  cursor: pointer;
+`
+
+const Animation = styled.div`
+  transition: transform .2s;
+
+  :hover {
+    transform: scale(2);
+  }
+`
+
+const Title = styled.h6`
+  font-size: 0.6rem;
+  margin: 0;
+  font-weight: 600;
+`
