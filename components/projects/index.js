e7a964cf6fd314243f8d9dad57a6259e0b96e198
francoromanolosada <francoromano1994@gmail.com>
2021-05-24 21:54:57 +0200

    Added ProjectsNavbar to export

--- components/projects/index.js
+++ components/projects/index.js
@@ -1,3 +1,4 @@
 export { default as Projects } from './Projects'
+export { default as ProjectsNavbar } from './ProjectsNavbar'
 export { default as projectItem } from './ProjectItem'
 export { default as ProjectsOrderer } from './ProjectsOrderer'
