6e02865653a5677feef0d2fb16e6047cfb094c08
francoromanolosada <francoromano1994@gmail.com>
2021-06-02 11:05:25 +0200

    Corrected component to make one request and fit object cover by parent size

--- components/projects/Icon.js
+++ components/projects/Icon.js
@@ -1,21 +1,33 @@
 import Image from 'next/image'
 import styled from 'styled-components'
 
-const Icon = ({ src, width, height, srcOnHover }) => {
+const Icon = ({ src, srcOnHover }) => {
   return (
-    <Ico
-      src={src}
-      width={width}
-      height={height}
-      onMouseOver={e => srcOnHover && (e.currentTarget.src = srcOnHover)}
-      onMouseOut={e => srcOnHover && (e.currentTarget.src = src || '')}
-    />
+    <Container>
+      <Ico src={src} visible />
+      <Ico src={srcOnHover} />
+    </Container>
   )
 }
 
 export default Icon
 
-const Ico = styled.img`
+const Container = styled.div`
   height: 100%;
   width: 100%;
+
+  :hover img:first-child {
+    display: none;
+  }
+  :hover img:last-child {
+    display: block;
+  }
+`
+
+const Ico = styled.img`
+  display: ${p => (p.visible ? 'block' : 'none')};
+  width: 100%;
+  height: 100%;
+
+  object-fit: cover;
 `
