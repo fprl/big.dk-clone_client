58a17e57eabae17adb0abf09289bab8be183cb94
francoromanolosada <francoromano1994@gmail.com>
2021-06-02 11:04:41 +0200

    Added Info component

---
+++ components/info/Info.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Info = () => {
+  return (
+    <>
+      <p>This project was created by Franco Romano Losada</p>
+      <a href="https://github.com/francoromanol">GitHub</a>
+    </>
+  )
+}
+
+export default Info
