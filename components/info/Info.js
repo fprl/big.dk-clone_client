2848068479bbf3874adfc54926eb279a1e4b4806
francoromanolosada <francoromano1994@gmail.com>
2021-06-03 14:36:14 +0200

    Added material-ui icons and corrected all components

--- components/info/Info.js
+++ components/info/Info.js
@@ -1,14 +1,43 @@
 import styled from 'styled-components'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
 
 const Info = () => {
   return (
-    <>
-      <p>This project was created by Franco Romano Losada</p>
-      <a href="https://github.com/francoromanol">GitHub</a>
-      <a href="https://github.com/francoromanol">GitHub</a>
-      <a href="https://github.com/francoromanol">GitHub</a>
-    </>
+    <FlexContainer>
+      <Text>made by <br /> Franco Romano Losada</Text>
+      <IconsContainer>
+        <a href="https://github.com/francoromanol" target="_blank">
+          <GitHubIcon fontSize="regular" />
+        </a>
+        <a href="https://twitter.com/Francoromanol" target="_blank">
+          <TwitterIcon fontSize="regular" />
+        </a>
+        <a href="https://www.linkedin.com/in/franco-romano-losada/" target="_blank">
+          <LinkedInIcon fontSize="regular" />
+        </a>
+      </IconsContainer>
+    </FlexContainer>
   )
 }
 
 export default Info
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  gap: 0.5rem;
+  width: 12rem;
+  height: 6rem;
+`
+
+const Text = styled.span`
+  font-size: 1rem;
+`
+
+const IconsContainer = styled(FlexContainer)`
+  flex-direction: row;
+  align-items: center;
+`
