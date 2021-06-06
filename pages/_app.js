5a52e989a5e88ee0916e977aa2caa134da86f5d9
francoromanolosada <francoromano1994@gmail.com>
2021-06-06 22:27:50 +0200

    Added meta viewport for scale

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import 'react-tippy/dist/tippy.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
