35c8fa4fa3e2ec9021e181a0ddbf2942acb9a3b1
francoromanolosada <francoromano1994@gmail.com>
2021-06-10 12:02:04 +0200

    Added global styles and reset to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import Head from 'next/head'
-import '../styles/globals.css'
 import 'react-tippy/dist/tippy.css'
 
+import GlobalStyles from '../styles/GlobalStyles'
+import Reset from '../styles/Reset'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Reset />
+      <GlobalStyles />
+      <Component {...pageProps} />    
+    </>
+  )
 }
 
 export default MyApp
